a44df179ff03defc82b425a8baca1a65d3fef0b1
stefidka <stef.vacheva@gmail.com>
2021-01-12 16:47:13 +0200

    “choose your own adventure” game similar to the App Store hit “Life Line” app.

---
+++ Destini/Destini/Model/Story.swift
@@ -0,0 +1,16 @@
+//
+//  Story.swift
+//  Destini
+//
+//  Created by Stefka Vacheva on 12.01.21.
+//
+
+import Foundation
+
+struct Story {
+    let title: String
+    let choice1: String
+    let choice1Destination: Int
+    let choice2: String
+    let choice2Destination: Int
+}
